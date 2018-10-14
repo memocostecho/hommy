39df55a496a9f1e172b1b2043472c96d023f7d49
Guillermo Rosales <guillermo.rosales@MACC02W20DEHTDD.local>
2018-10-13 17:42:31 -0700

    Set up mvvm arch.

--- app/src/main/java/com/guillermorosales/smarthome/MainActivity.kt
+++ app/src/main/java/com/guillermorosales/smarthome/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.guillermorosales.smarthome
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
+import com.jakewharton.rxbinding2.view.RxView
+import com.jakewharton.rxbinding2.widget.RxCompoundButton
+import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -11,16 +17,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        switch_lamp.setOnCheckedChangeListener {_, checked -> doSomething(checked)}
+        bindViewModel()
     }
 
-    private fun doSomething(checked: Boolean) {
-        // Write a message to the database
-        val database = FirebaseDatabase.getInstance()
-        val myRef = database.reference
+    private fun bindViewModel() {
+        val model = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        model.getLightState().observe(this, Observer<Boolean> { state ->
+            switch_lamp.isChecked = state!!
+        })
 
-        myRef.child("lights").child("on").setValue(checked)
-
-
+        RxCompoundButton.checkedChanges(switch_lamp).subscribe {
+            model.setLightState(it)
+        }
     }
 }
